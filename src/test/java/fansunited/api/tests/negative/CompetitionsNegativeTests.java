22d7f72f8ca83099c6de2df9181099a209732e14
veskoatanasow <veskoatanasov00@gmail.com>
2025-05-08 19:31:52 +0300

    removed unused import

--- src/test/java/fansunited/api/tests/negative/CompetitionsNegativeTests.java
+++ src/test/java/fansunited/api/tests/negative/CompetitionsNegativeTests.java
@@ -9,7 +9,8 @@ import utils.dto.request.CompetitionRequestDto;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CompetitionsNegativeTests extends BaseTest {
 
